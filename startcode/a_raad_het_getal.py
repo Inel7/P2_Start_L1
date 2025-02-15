a2e92bd50a46348a6c3a30b1099006d85b76ae37
Inel7 <indyelens@gmail.com>
2025-02-15 22:16:48 +0100

    P2_L2

--- startcode/a_raad_het_getal.py
+++ startcode/a_raad_het_getal.py
@@ -1,5 +1,25 @@
 # Schrijf een functie is_getal_geraden die op basis van een gok en een geheim_nummer controleert of de gok correct was.
 # Print hier een boodschap, en return de juiste waarde.
-
-
 # Schrijf een functie raad_het_getal, die op basis van een bovengrens het raadspel zal spelen.
+
+import random
+from random import randint
+
+
+def is_getal_geraden(gok,geheim_getal):
+    if gok == geheim_getal:
+        print("Proficiat, je hebt het getal geraden")
+        return True
+    else:
+        print("fout")
+        return False
+
+def raad_het_getal(bovengrens):
+    geheim_getal = randint(1,bovengrens)
+    geraden = False
+
+    while not geraden:
+        gok = int(input(f"geef een getal tussen 1 en {bovengrens} "))
+        geraden = is_getal_geraden(gok,geheim_getal)
+
+raad_het_getal(10)
--- startcode/tictactoe/mytictactoe.py
+++ startcode/tictactoe/mytictactoe.py
@@ -1,3 +1,6 @@
+from xml.etree.ElementTree import XMLID
+
+
 def initialiseer_bord():
     # bord = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     bord = []
@@ -8,26 +11,93 @@ def initialiseer_bord():
         bord.append(rij_inhoud)
     return bord
 
-
-def zet():
-    pass
-
-
 def print_bord(bord):
-    pass
+    print()
+    for rij in bord:
+        print("|".join(rij))
+        print('_'*5)
 
+def zet(bord,rij,kolom,speler):
+    bord[rij][kolom] = speler
+    return bord
 
 def controleer_winnaar(bord):
-    pass
+    if controleer_diagonaal(bord) or controleer_verticaal(bord) or controleer_diagonaal(bord):
+        return True
+    return False
+
 
 
 def controleer_horizontaal(bord):
-    pass
+    for rij in bord:
+        if rij[0]==rij[1]==rij[2] != ' ':
+            return True
+        return False
 
 
 def controleer_verticaal(bord):
-    pass
+    for kolom in range(3):
+        if bord[1][kolom]==bord[0][kolom]==bord[2][kolom] != ' ':
+            return True
+        return False
 
 
 def controleer_diagonaal(bord):
-    pass
+        if bord[0][0] == bord[1][1] == bord[2][2] != ' ':
+            return True
+        if bord[0][2] == bord[1][1] == bord[2][0] != ' ':
+            return True
+        return False
+def controleer_input(bord,rij,kolom):
+    rij_is_geldig = 0 <= rij <= 2  # Geldig rijnummer
+    kolom_is_geldig = 0 <= kolom <= 2  # Geldig kolomnummer
+
+    if rij_is_geldig and kolom_is_geldig:
+        if bord[rij][kolom] == ' ':
+            return True
+    return False
+
+
+def tic_tac_toe():
+    bord= initialiseer_bord()
+    huidige_speler = 'X'
+    einde_spel = False
+    winnaar = ''
+    teller = 0
+    while not einde_spel:
+
+        if controleer_input(bord,rij,kolom):
+            print_bord(bord)
+            rij = int(input("kies een rij(0,1 of 2) "))
+            kolom = int(input("kies een kolom(0,1 of 2) "))
+
+            bord= zet(bord,rij,kolom,huidige_speler)
+            einde_spel = controleer_winnaar(bord)
+            teller += 1
+            if einde_spel:
+                winnaar = huidige_speler
+            if huidige_speler == 'X':
+                huidige_speler = 'O'
+            else:
+                huidige_speler = 'X'
+        else:
+            print('ongeldige zet ')
+        if teller == 9:
+            einde_spel = True
+
+    print_bord(bord)
+
+    if winnaar == '':
+         print("Het spel eindigt in gelijkspel.")
+    else:
+        print(f"Speler {winnaar} heeft gewonnen!")
+tic_tac_toe()
+
+
+
+
+
+
+
+
+
